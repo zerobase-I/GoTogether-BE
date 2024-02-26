d7f4c69e4bc3107a74812127baadafda8fe75f6a
qoreh <qoreh923@gmail.com>
2024-02-26 22:40:25 +0900

    chore: 초기 설정

--- src/main/java/com/example/gotogetherbe/GoTogetherBeApplication.java
+++ src/main/java/com/example/gotogetherbe/GoTogetherBeApplication.java
@@ -1,6 +1,5 @@
 package com.example.gotogetherbe;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
