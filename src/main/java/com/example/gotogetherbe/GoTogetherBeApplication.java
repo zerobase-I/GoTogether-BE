c3ae2e20883164a71761a964d7febf2a8f31ac65
qoreh <qoreh923@gmail.com>
2024-02-26 22:39:23 +0900

    chore: 초기 설정

---
+++ src/main/java/com/example/gotogetherbe/GoTogetherBeApplication.java
@@ -0,0 +1,14 @@
+package com.example.gotogetherbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoTogetherBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoTogetherBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gotogetherbe/GoTogetherBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gotogetherbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoTogetherBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
