1260c41ba927fa3f46009f6c990d83a07c596d3d
Namgyu11 <hagaji3@naver.com>
2024-02-29 05:12:02 +0900

    [feat]: LoginUser 인터페이스 생성

---
+++ src/main/java/com/example/gotogetherbe/auth/config/LoginUser.java
@@ -0,0 +1,15 @@
+package com.example.gotogetherbe.auth.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 현재 로그인한 사용자의 정보를 가져오는데 사용
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+
+}
