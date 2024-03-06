234c9c09f68ca4faa5236d6f707ab153c4217d8e
qoreh <qoreh923@gmail.com>
2024-03-07 04:46:43 +0900

    [feat]: Query DSL, 리뷰어, 타겟멤버가 동일한 채팅방에 속해있는지 확인하는 메서드

--- src/main/java/com/example/gotogetherbe/chat/repository/ChatMemberRepository.java
+++ src/main/java/com/example/gotogetherbe/chat/repository/ChatMemberRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatMemberRepository extends JpaRepository<ChatMember, Long> {
+public interface ChatMemberRepository extends JpaRepository<ChatMember, Long>, CustomChatMemberRepository {
   Optional<ChatMember> findByChatRoomIdAndMemberId(Long chatRoomId, Long memberId);
 
   List<ChatMember> findAllByChatRoomId(Long chatRoomId);
---
+++ src/main/java/com/example/gotogetherbe/chat/repository/impl/CustomChatMemberRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.example.gotogetherbe.chat.repository.impl;
+
+import com.example.gotogetherbe.chat.entity.QChatMember;
+import com.example.gotogetherbe.chat.repository.CustomChatMemberRepository;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CustomChatMemberRepositoryImpl implements CustomChatMemberRepository {
+
+    private final JPAQueryFactory jpa;
+    @Override
+    public boolean areUsersInSameChatRoom(Long reviewerId, Long targetMemberId, Long chatRoomId) {
+        QChatMember chatMember = QChatMember.chatMember;
+        BooleanExpression condition = chatMember.chatRoom.id.eq(chatRoomId)
+                .and(chatMember.member.id.in(reviewerId, targetMemberId));
+
+        Long count = jpa.select(chatMember.count())
+                .from(chatMember)
+                .where(condition)
+                .fetchOne();
+
+        return count != null && count == 2L;
+    }
+}
