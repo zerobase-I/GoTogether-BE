2e690bdac9e734934b7f2e77a2b7453eb5ac73cd
Namgyu11 <hagaji3@naver.com>
2024-03-06 20:13:18 +0900

    [fix]: merge 충동 해결

--- src/main/java/com/example/gotogetherbe/member/entitiy/Member.java
+++ src/main/java/com/example/gotogetherbe/member/entitiy/Member.java
@@ -1,5 +1,6 @@
 package com.example.gotogetherbe.member.entitiy;
 
+import com.example.gotogetherbe.accompany.review.entity.TravelScore;
 import com.example.gotogetherbe.global.entity.BaseEntity;
 import com.example.gotogetherbe.member.entitiy.type.MemberLoginType;
 import com.example.gotogetherbe.member.entitiy.type.MemberGender;
@@ -11,18 +12,20 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
