23023e37a184c7332234f849f575fcc74ac43eab
qoreh <qoreh923@gmail.com>
2024-03-07 04:51:02 +0900

    [refactor]: unused import 제거

--- src/main/java/com/example/gotogetherbe/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/example/gotogetherbe/chat/repository/ChatRoomRepository.java
@@ -1,7 +1,6 @@
 package com.example.gotogetherbe.chat.repository;
 
 import com.example.gotogetherbe.chat.entity.ChatRoom;
-import com.example.gotogetherbe.post.entity.Post;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
