d58d653ba5505ce16172b2b8620804ec8d1c2811
Namgyu11 <hagaji3@naver.com>
2024-03-08 17:19:46 +0900

    [feat] : 게시물 생성 시 이미지 첨부 여부 로직 구현

--- src/main/java/com/example/gotogetherbe/global/util/aws/dto/S3ImageDto.java
+++ src/main/java/com/example/gotogetherbe/global/util/aws/dto/S3ImageDto.java
@@ -2,12 +2,24 @@ package com.example.gotogetherbe.global.util.aws.dto;
 
 
 import com.example.gotogetherbe.global.util.aws.entity.PostImage;
+import lombok.Builder;
 
+@Builder
 public record S3ImageDto(
+
+    Long id,
     String url,
     String fileName,
     Long size
 ) {
+  public static S3ImageDto fromEntity(PostImage postImage) {
+    return S3ImageDto.builder()
+        .id(postImage.getId())
+        .url(postImage.getUrl())
+        .fileName(postImage.getFileName())
+        .size(postImage.getSize())
+        .build();
+  }
   public PostImage toEntity() {
     return PostImage.builder()
         .url(url)
