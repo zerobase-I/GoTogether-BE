ce4ecc9e6de8708e2bfd96c1fa04b3f719e0d6ca
qoreh <qoreh923@gmail.com>
2024-03-12 19:38:45 +0900

    feat: send(알림 전송 기능) 구현

--- src/main/java/com/example/gotogetherbe/notification/service/NotificationService.java
+++ src/main/java/com/example/gotogetherbe/notification/service/NotificationService.java
@@ -1,10 +1,9 @@
 package com.example.gotogetherbe.notification.service;
 
-import com.example.gotogetherbe.global.exception.GlobalException;
-import com.example.gotogetherbe.global.exception.type.ErrorCode;
-import com.example.gotogetherbe.member.entitiy.Member;
-import com.example.gotogetherbe.member.repository.MemberRepository;
+import com.example.gotogetherbe.notification.dto.NotificationInfoDto;
+import com.example.gotogetherbe.notification.entity.Notification;
 import com.example.gotogetherbe.notification.repository.EmitterRepository;
+import com.example.gotogetherbe.notification.repository.NotificationRepository;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Objects;
@@ -18,8 +17,8 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RequiredArgsConstructor
 public class NotificationService {
 
-    private final MemberRepository memberRepository;
     private final EmitterRepository emitterRepository;
+    private final NotificationRepository notificationRepository;
     private static final long DEFAULT_TIMEOUT = 60L * 1000 * 60;
 
     /**
@@ -30,11 +29,8 @@ public class NotificationService {
      * @return 연결된 SseEmitter
      */
     public SseEmitter subscribe(String email, String lastEventId) {
-        Member member = memberRepository.findByEmail(email)
-            .orElseThrow(() -> new GlobalException(ErrorCode.USER_NOT_FOUND));
-
         // lastEventId가 nulld이 아닐때 데이터가 유실된 시점 파악을 위해 current time을 이용하여 emitterId 생성
-        String emitterId = member.getId() + "_" + System.currentTimeMillis();
+        String emitterId = email + "_" + System.currentTimeMillis();
         SseEmitter emitter = emitterRepository.save(emitterId, new SseEmitter(DEFAULT_TIMEOUT));
 
         // emitter delete
@@ -43,20 +39,33 @@ public class NotificationService {
         emitter.onError((e) -> emitterRepository.deleteById(emitterId));
 
         // 503 error 방지 더미데이터 전송
-        sendToClient(emitter, emitterId, "EventStream Created. [memberId=" + member.getId() + "]");
+        sendToClient(emitter, emitterId, "EventStream Created. [userEmail=" + email + "]");
 
         // lastEventId가 존재할 경우 해당 이벤트부터 전송
         if (Objects.nonNull(lastEventId) && !lastEventId.isEmpty()) {
-            Map<String, Object> eventCache = emitterRepository
-                .findAllEventCacheStartWithByMemberId(member.getId() + "_");
-            eventCache.entrySet().stream()
-                .filter(
-                    entry -> lastEventId.compareTo(entry.getKey()) < 0) // lastEventId 이후 데이터만 전송
-                .forEach(entry -> sendToClient(emitter, entry.getKey(), entry.getValue()));
+            sendLostData(email, lastEventId, emitter);
         }
 
         return emitter;
     }
+
+    /**
+     * 알림 전송
+     * @param notificationInfo 알림 정보
+     */
+    public void send(NotificationInfoDto notificationInfo) {
+        Notification notification = notificationRepository.save(notificationInfo.of()); //repository에 저장
+        log.info("알림 저장 완료");
+
+        Map<String, SseEmitter> emitters = emitterRepository
+            .findAllEmitterStartWithByEmail(notification.getMember().getEmail() + "_"); // 연결된 emitter 조회
+
+        emitters.forEach((key, emitter) -> {
+            emitterRepository.saveEventCache(key, notification.getId()); // event cache 저장(notification id만 저장)
+            sendToClient(emitter, key, "New Notification");
+        });
+    }
+
 
     /**
      * 클라이언트로 데이터 전송
@@ -70,10 +79,28 @@ public class NotificationService {
             emitter.send(SseEmitter.event()
                 .id(emitterId)
                 .data(data));
+            log.info("알림 전송 완료");
         } catch (IOException e) {
             emitterRepository.deleteById(emitterId);
             emitter.completeWithError(e);
+            log.info("알림 전송 실패. userEmail: {}", emitterId.split("_")[0]);
         }
     }
+
+    /**
+     * 유실된 데이터 전송
+     * @param email 사용자 email
+     * @param lastEventId 마지막으로 수신한 이벤트 아이디
+     * @param emitter emitter
+     */
+    private void sendLostData(String email, String lastEventId, SseEmitter emitter) {
+        Map<String, Object> eventCache = emitterRepository
+            .findAllEventCacheStartWithByEmail(email + "_");
+        eventCache.entrySet().stream()
+            .filter(
+                entry -> lastEventId.compareTo(entry.getKey()) < 0) // lastEventId 이후 데이터만 전송
+            .forEach(entry -> sendToClient(emitter, entry.getKey(), entry.getValue()));
+    }
+
 
 }
