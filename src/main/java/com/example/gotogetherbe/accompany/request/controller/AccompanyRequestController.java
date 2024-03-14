bae8aebb39a72e224ba8e81f769630c11071643e
qoreh <qoreh923@gmail.com>
2024-03-15 05:01:19 +0900

    refactor: 와일드카드 제거

--- src/main/java/com/example/gotogetherbe/accompany/request/controller/AccompanyRequestController.java
+++ src/main/java/com/example/gotogetherbe/accompany/request/controller/AccompanyRequestController.java
@@ -1,8 +1,10 @@
 package com.example.gotogetherbe.accompany.request.controller;
 
+import com.example.gotogetherbe.accompany.request.dto.AccompanyRequestDto;
 import com.example.gotogetherbe.accompany.request.dto.AccompanyRequestSendDto;
 import com.example.gotogetherbe.accompany.request.service.AccompanyRequestService;
 import com.example.gotogetherbe.auth.config.LoginUser;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,38 +22,35 @@ public class AccompanyRequestController {
     private final AccompanyRequestService accompanyRequestService;
 
     @PostMapping("/send")
-    public ResponseEntity<?> sendAccompanyRequest(
+    public ResponseEntity<AccompanyRequestDto> sendAccompanyRequest(
         @LoginUser String username,
         @RequestBody AccompanyRequestSendDto accompanyRequestSendDto
     ) {
-        return ResponseEntity.ok()
-            .body(accompanyRequestService.sendAccompanyRequest(username, accompanyRequestSendDto));
+        return ResponseEntity.ok(accompanyRequestService
+            .sendAccompanyRequest(username, accompanyRequestSendDto));
     }
 
     @GetMapping("/send")
-    public ResponseEntity<?> sentAccompanyRequest(@LoginUser String username) {
-        return ResponseEntity.ok().body(accompanyRequestService.getSentAccompanyRequests(username));
+    public ResponseEntity<List<AccompanyRequestDto>> sentAccompanyRequest(@LoginUser String username) {
+        return ResponseEntity.ok(accompanyRequestService.getSentAccompanyRequests(username));
     }
 
     @GetMapping("/receive")
-    public ResponseEntity<?> receivedAccompanyRequest(@LoginUser String username) {
-        return ResponseEntity.ok()
-            .body(accompanyRequestService.getReceivedAccompanyRequests(username));
+    public ResponseEntity<List<AccompanyRequestDto>> receivedAccompanyRequest(@LoginUser String username) {
+        return ResponseEntity.ok(accompanyRequestService.getReceivedAccompanyRequests(username));
     }
 
     @PostMapping("/approve/{requestId}")
-    public ResponseEntity<?> approveAccompanyRequest(
-        @LoginUser String username,
-        @PathVariable Long requestId
+    public ResponseEntity<AccompanyRequestDto> approveAccompanyRequest(
+        @LoginUser String username, @PathVariable Long requestId
     ) {
         return ResponseEntity.ok()
             .body(accompanyRequestService.approveAccompanyRequest(username, requestId));
     }
 
     @PostMapping("/reject/{requestId}")
-    public ResponseEntity<?> rejectAccompanyRequest(
-        @LoginUser String username,
-        @PathVariable Long requestId
+    public ResponseEntity<AccompanyRequestDto> rejectAccompanyRequest(
+        @LoginUser String username, @PathVariable Long requestId
     ) {
         return ResponseEntity.ok()
             .body(accompanyRequestService.rejectAccompanyRequest(username, requestId));
