bfddd2b284f2babaa044915629c2a677310ae052
Namgyu11 <hagaji3@naver.com>
2024-03-16 23:02:19 +0900

    [refactor] - 외부 로그인을 위한 password 컬럼 nullable = true 설정

--- src/main/java/com/example/gotogetherbe/member/entitiy/Member.java
+++ src/main/java/com/example/gotogetherbe/member/entitiy/Member.java
@@ -44,7 +44,6 @@ public class Member extends BaseEntity {
   @Column(nullable = false, unique = true, length = 30)
   private String email;
 
-  @Column(nullable = false)
   private String password;
 
   @Column(nullable = false, length = 10)
@@ -80,7 +79,7 @@ public class Member extends BaseEntity {
   private String description;
 
   @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-  @JoinColumn(name = "travel_score_id", nullable = true)
+  @JoinColumn(name = "travel_score_id")
   private TravelScore travelScore;
 
   @Builder.Default
