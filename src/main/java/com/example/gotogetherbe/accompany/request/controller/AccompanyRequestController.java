8206f2e425a43e2f7f162f88084278d32ba121f7
qoreh <qoreh923@gmail.com>
2024-03-16 16:21:28 +0900

    refactor: unused 제거

--- src/main/java/com/example/gotogetherbe/accompany/request/controller/AccompanyRequestController.java
+++ src/main/java/com/example/gotogetherbe/accompany/request/controller/AccompanyRequestController.java
@@ -1,7 +1,6 @@
 package com.example.gotogetherbe.accompany.request.controller;
 
 import com.example.gotogetherbe.accompany.request.dto.AccompanyStatusDto;
-import com.example.gotogetherbe.accompany.request.dto.AccompanyRequestDto;
 import com.example.gotogetherbe.accompany.request.service.AccompanyStatusService;
 import com.example.gotogetherbe.auth.config.LoginUser;
 import java.util.List;
@@ -10,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
