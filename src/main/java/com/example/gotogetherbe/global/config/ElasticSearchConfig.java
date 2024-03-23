688fa81e8c6c78dac07d9692a03dce3acf9b6c79
Namgyu11 <hagaji3@naver.com>
2024-03-24 00:14:51 +0900

    [refactor]: ES uris 설정

--- src/main/java/com/example/gotogetherbe/global/config/ElasticSearchConfig.java
+++ src/main/java/com/example/gotogetherbe/global/config/ElasticSearchConfig.java
@@ -2,6 +2,7 @@ package com.example.gotogetherbe.global.config;
 
 import com.example.gotogetherbe.post.repository.PostSearchRepository;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
@@ -12,11 +13,14 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @Configuration
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
+  @Value("${spring.elasticsearch.uris}")
+  private String uris;
+
 
   @Override
   public RestHighLevelClient elasticsearchClient() {
     ClientConfiguration configuration = ClientConfiguration.builder().
-        connectedTo("localhost:9200").build();
+        connectedTo(uris).build();
     return RestClients.create(configuration).rest();
   }
 }
