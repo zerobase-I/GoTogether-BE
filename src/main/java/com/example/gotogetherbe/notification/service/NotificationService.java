be3ff98ef7b41fbc00edda4163b5ed16cf1ae24b
qoreh <qoreh923@gmail.com>
2024-03-25 02:55:37 +0900

    refactor: unused 제거

--- src/main/java/com/example/gotogetherbe/notification/service/NotificationService.java
+++ src/main/java/com/example/gotogetherbe/notification/service/NotificationService.java
@@ -16,7 +16,6 @@ import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Slf4j
